b18d145a99842824c10b439b46beb51d05005ffa
Manikandan4080 <113871926+Manikandan4080@users.noreply.github.com>
2023-11-15 08:22:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // your code here
+let url = document.getElementById("url");
+url.innerText = "https://localhost:8080/";
+
+let btn = document.getElementById("button");
+
+function changeText(){
+  let name = document.getElementById("name").value;
+	let year = document.getElementById("year").value;
+	let ans = "https://localhost:8080/";
+	if(name){
+		ans += `?name=${name}`;
+	}
+	if(year){
+		if(name){
+			ans += `&?year=${year}`;
+		}
+		else{
+			ans += `?year=${year}`
+		}
+	}
+
+	url.innerText = ans;
+}
